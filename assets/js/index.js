19068f8d003d6a489d1e46a14bbe67cec98933ad
Ariel Enferrel <enferrelariel@hotmail.com>
2023-03-15 14:34:36 -0300

    termine el index

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,9 @@ createApp({
 data() {
 return {
   dataEvents:[],
-  checks:[],
+  checked:[],
+  checkBox:[],
+  eventFiltrado:[],
   textSearch:"",
 message: '¡Hola, Mundo!'
 }
@@ -14,12 +16,23 @@ created(){
   fetch("https://mindhub-xj03.onrender.com/api/amazing")
   .then(response=>response.json())
   .then(datos=> {this.dataEvents=datos
-  console.log(this.dataEvents)})
+  console.log(this.dataEvents)
+this.checkBox=[... new Set(this.dataEvents.events.map(event=>event.category))]})
+
   .catch(error=>console.log(error.message))
   console.log(this.message)
 },
 methods:{
 
+},
+computed:{
+  filtrarPorCheck:function filtroCheck(){
+   let eventCheck=this.checked.length==0?this.dataEvents.events:
+    this.dataEvents.events.filter(evento=>
+      this.checked.includes(evento.category))
+     this.eventFiltrado= this.textSearch==""?eventCheck: eventCheck.filter(event=>event.name.toLowerCase().search(this.textSearch.toLowerCase().trim())!=-1)
+     return this.eventFiltrado
+  }
 }
 //inicializa la instancia de Vue
 }).mount('#app')
